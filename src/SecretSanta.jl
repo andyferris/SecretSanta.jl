3bf4a32ac114c72ec8a1c2337a9e2012c7ef936e
Andy Ferris <andy.ferris@roames.com.au>
2017-11-25 23:04:21 +1000

    Add `secret_santa()`

--- src/SecretSanta.jl
+++ src/SecretSanta.jl
@@ -1,5 +1,42 @@
 module SecretSanta
 
-# package code goes here
+function secret_santa(people::Array{String}, path::String = ENV["HOME"])
+    n_people = length(people)
+
+    if n_people < 2
+        error("Need to provide at least two people to exchange gifts")
+    end
+    
+    srand()
+    perm = randperm(n_people)
+    while some_same(perm) # Make sure no-one gifts themselves
+        perm = randperm(n_people)
+    end
+
+    for i ∈ 1:n_people
+        giver = people[i]
+        receiver = people[perm[i]]
+
+        open(joinpath(path, "$(giver).txt"), "w") do io
+            write(io, """
+                Dear $(giver),
+                
+                For Secret Santa, please provide a gift for $(receiver).
+
+                Kind regards,
+                Julia
+                """)
+        end
+    end
+end
+
+function some_same(perm)
+    for i ∈ 1:length(perm)
+        if i == perm[i]
+            return true
+        end
+    end
+    return false
+end
 
 end # module
